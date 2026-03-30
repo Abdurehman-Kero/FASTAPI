793d225711d6b1fa77c5f7502d9b04139ca212bf
abdurehman-kero <keroabdurehman@gmail.com>
2026-03-30 15:38:20 -0400

    main file updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 from pydantic import BaseModel
 # 1. Create the "app" instance
 app = FastAPI()
@@ -20,6 +21,8 @@ def get_user_info(name: str, age: int):
 class Item(BaseModel):
     name: str
     price: float
+    description: Optional[str] = None  # This is optional
+    tax: float = 0.0  # This is optional, defaults to 0.0
     is_offer: bool = False  # This is optional, defaults to False
 @app.post("/items/")
 def create_item(item: Item):
