73e326722ad06230d1c29dcba9833ed2d35a02b3
abdurehman-kero <keroabdurehman@gmail.com>
2026-03-30 16:00:27 -0400

    virtual sample database added

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-from typing import Optional
+
+from typing import Optional, List  
 from pydantic import BaseModel
 # 1. Create the "app" instance
 app = FastAPI()
+my_database = []
 
+# This is your "Template" or Blueprint
+class Item(BaseModel):
+    name: str
+    price: float
+    description: Optional[str] = None  # This is optional
+    tax: float = 0.0  # This is optional, defaults to 0.0
+    is_offer: bool = False  # This is optional, defaults to False
 # 2. Define a "route"
 @app.get("/")
 def read_root():
@@ -17,18 +26,18 @@ def get_user_info(name: str, age: int):
         "message": f"Hello {name}!",
         "age_next_year": age + 1
     }
-# This is your "Template" or Blueprint
-class Item(BaseModel):
-    name: str
-    price: float
-    description: Optional[str] = None  # This is optional
-    tax: float = 0.0  # This is optional, defaults to 0.0
-    is_offer: bool = False  # This is optional, defaults to False
 @app.post("/items/")
 def create_item(item: Item):
     total_price = item.price + item.tax
+    my_database.append(item)
     return {
         "message": f"Item '{item.name}' created successfully!",
         "item_details": item,
-        "total_with_tax": total_price
+        "total_with_tax": total_price,
+        # "message": f"Item '{item.name}' created successfully!",
+       "current_db_size": len(my_database)
     }
+
+@app.get("/all-items/")
+def get_all_items():
+    return {"all_items": my_database}
